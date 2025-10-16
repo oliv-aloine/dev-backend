e9fb8a924139680704a67a487f37b81139e14d07
aline <aline.deoliveira0806@gmail.com>
2025-10-16 21:03:13 +0100

    add controller and create view

---
+++ dev-backend/Controllers/VeiculosController.cs
@@ -0,0 +1,39 @@
+﻿using dev_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dev_backend.Controllers;
+
+public class VeiculosController : Controller
+{
+    private readonly AppDbContext _context;
+    public VeiculosController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var dados = await _context.Veiculos.ToListAsync();
+        
+        return View(dados);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(Veiculo veiculo)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Veiculos.Add(veiculo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        
+        return View();
+    }
+}
--- dev-backend/Models/Veiculo.cs
+++ dev-backend/Models/Veiculo.cs
@@ -16,8 +16,10 @@ public class Veiculo
     public string Placa { get; set; }
 
     [Required(ErrorMessage = "Ano de Fabricação obrigatório.")]
+    [Display(Name="Ano de Fabricação")]
     public int AnoFabricacao { get; set; }
     
     [Required(ErrorMessage = "Ano do Modelo obrigatório.")]
+    [Display(Name="Ano do Modelo")]
     public int AnoModelo { get; set; }
 }
