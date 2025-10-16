1f80774b056a53b070dda4a5f9d4ead2e7dedf84
aline <aline.deoliveira0806@gmail.com>
2025-10-16 14:59:24 +0100

    remove pastas não  utilizadas

---
+++ dev-backend/Models/Veiculo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dev_backend.Models;
+
+[Table("Veiculos")]
+public class Veiculo
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Nome obrigatório.")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "Placa obrigatório.")]
+    public string Placa { get; set; }
+
+    [Required(ErrorMessage = "Ano de Fabricação obrigatório.")]
+    public int AnoFabricacao { get; set; }
+    
+    [Required(ErrorMessage = "Ano do Modelo obrigatório.")]
+    public int AnoModelo { get; set; }
+}
